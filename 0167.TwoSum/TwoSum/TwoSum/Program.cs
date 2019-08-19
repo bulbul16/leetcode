5bcda3d5f2dc4d11cf13fec4d93da8873bcf975e
mahedee <mahedee.hasan@gmail.com>
2019-08-19 18:16:37 +0600

    done 167

--- 0167.TwoSum/TwoSum/TwoSum/Program.cs
+++ 0167.TwoSum/TwoSum/TwoSum/Program.cs
@@ -6,13 +6,30 @@ namespace TwoSum
     {
         public int[] TwoSum(int[] numbers, int target)
         {
-            //if(target >= )
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                int a = numbers[i];
+                int b = target - a;
+
+                int index = FindIndex(numbers, 0, numbers.Length - 1, b);
+
+                if(index != -1 && index != i)
+                {
+                    //Arrange in ascending order
+                    if(index < i)
+                    {
+                        return new int[] { index + 1, i + 1 };
+                    }
+                    return new int[] { i + 1, index + 1 };
+                }
+            }
             return new int[2];
         }
 
+        // Binary search
         public int FindIndex(int[] data, int lowerBound, int upperBound, int item)
         {
-            if (lowerBound > upperBound) return lowerBound;
+            if (lowerBound > upperBound) return -1;
             int mid = (lowerBound + upperBound) / 2;
 
             if (data[mid] == item) return mid;
@@ -30,10 +47,13 @@ namespace TwoSum
     {
         static void Main(string[] args)
         {
-            int[] numbers = {1, 3, 5, 6};
-            int position = new Solution().FindIndex(numbers, 0, numbers.Length-1, 7);
+            //int[] numbers = { 2, 7, 11, 15 };
+            //int[] positions = new Solution().TwoSum(numbers, 19);
 
-            Console.WriteLine(position);
+            int[] numbers = { 1, 2, 3, 4, 4, 9, 56, 90 };
+            int[] positions = new Solution().TwoSum(numbers, 8);
+
+            //Console.WriteLine(position);
             Console.ReadKey();
             //Console.WriteLine("Hello World!");
         }
